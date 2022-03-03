492485b1a63ad3c0b27cb71a512770eeffdb6809
Venu94900 <venugopaledam666@gmail.com>
2022-03-03 17:53:35 +0530

    This is my first commit

---
+++ src/test/java/com/inetbanking/testCases/TC_LoginTest_001.java
@@ -0,0 +1,42 @@
+package com.inetbanking.testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.inetbanking.pageObjects.LoginPage;
+
+
+public class TC_LoginTest_001 extends BaseClass
+{
+
+	@Test
+	public void logintest()
+	{
+		
+		
+		logger.info("URL is opened");
+		
+		LoginPage lp=new LoginPage(driver);
+		lp.setUsername(username);
+		logger.info("Entered  username");
+		lp.setPassword(password);
+		logger.info("Entered  password");
+		lp.clickSubmit();
+		
+		
+		if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
+		{
+			Assert.assertTrue(true);
+			logger.info("Login test passed");
+		}
+		else
+		{
+           Assert.assertTrue(false);
+           logger.info("Login test failed");
+		}
+		
+		
+	}
+	
+	
+}
